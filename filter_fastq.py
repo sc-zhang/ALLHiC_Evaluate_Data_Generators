dcee91f944399c0f4486a7e4428f39fac77e6441
sc-zhang <zsc-zhang@foxmail.com>
2023-06-03 16:38:04 +0800

    fix bug

--- filter_fastq.py
+++ filter_fastq.py
@@ -3,41 +3,29 @@ import sys
 import gc
 
 
-def is_in_list(filter_list, id):
-	s = 0
-	e = len(filter_list)-1
-	while s<=e:
-		mid = (s+e)/2
-		if filter_list[mid] > id:
-			e = mid-1
-		elif filter_list[mid] < id:
-			s = mid+1
-		else:
-			return True
-	return False
-
-
 def filter_fastq(in_fq, in_list, out_fq):
-	filter_list = []
+	filter_set = set()
 	print("Reading filter list")
 	with open(in_list, 'r') as fin:
 		for line in fin:
-			filter_list.append(line.strip())
+			filter_set.add(line.strip())
 	
-	sorted_filter_list = sorted(filter_list)
 	fq_db = {}
 	print("Reading fastq")
 	with open(in_fq, 'r') as fin:
+        id = ""
+        cnt = 0
 		for line in fin:
-			if line[0] == '@' and len(fq_db[id])==4:
+			if line[0] == '@' and cnt%4==0:
 				id = line[1:].strip().split()[0]
 				fq_db[id] = []
 			fq_db[id].append(line)
+            cnt += 1
 	
 	print("Writing fastq")
 	with open(out_fq, 'w') as fout:
 		for id in sorted(fq_db):
-			if is_in_list(sorted_filter_list, id) == False:
+			if not id in filter_set:
 				fout.write(''.join(fq_db[id]))
 	del fq_db
 	gc.collect()
